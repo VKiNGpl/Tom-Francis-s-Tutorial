3b77f3c0f6fcf608888053ee01a6b1307d19a426
Krystian Spiewak <krystianspiewak@gmail.com>
2020-07-23 15:41:45 -0500

    Guards and patrol behavior implemented.

--- Assets/EnemyBehavior.cs
+++ Assets/EnemyBehavior.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     public float speed;
+    public float turnSpeed;
     public float spawnRate;
     public float damageInterval;
     public float damageRate;
+    public float visionRange;
+    public float visionConeAngle;
+    public bool playerSpotted;
+    public Light myLight;
 
     float secondsSinceSpawn;
-    //float secondsSinceDamage;
+
+    Rigidbody ourRigidbody;
     // Start is called before the first frame update
     void Start()
     {
         secondsSinceSpawn = 0.0f;
-        //secondsSinceDamage = 0.0f;
+        playerSpotted = false;
+        myLight.color = Color.white;
+        ourRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,12 +33,36 @@ public class EnemyBehavior : MonoBehaviour
 
         if (References.thePlayer != null)
         {
-            Rigidbody ourRigidbody = GetComponent<Rigidbody>();
-            Vector3 vectorToPlayer = References.thePlayer.transform.position - transform.position;
-            ourRigidbody.velocity = vectorToPlayer.normalized * speed;
+            Vector3 playerPosition = References.thePlayer.transform.position;
+            Vector3 vectorToPlayer = playerPosition - transform.position;
+
+            if (playerSpotted)
+            {
+                //Follow the player
+                
+                ourRigidbody.velocity = vectorToPlayer.normalized * speed;
+                Vector3 playerPositionatOurHeight = new Vector3(playerPosition.x, transform.position.y, playerPosition.z);
+                transform.LookAt(playerPositionatOurHeight);
+                myLight.color = Color.red;
+            }
+            else
+            {
+                //Rotate
+                Vector3 lateralOffset = transform.right * Time.deltaTime * turnSpeed;
+                transform.LookAt(transform.position + transform.forward + lateralOffset);
+                ourRigidbody.velocity = transform.forward * speed;
+                //Check if player is in vision cone
+                if (Vector3.Distance(transform.position, playerPosition) <= visionRange)
+                {
+                    if (Vector3.Angle(transform.forward, vectorToPlayer) <= visionConeAngle)
+                    {
+                        playerSpotted = true;
+                    }
+                }
+            }
         }
 
-        if (secondsSinceSpawn >= spawnRate)
+        if (secondsSinceSpawn >= spawnRate && spawnRate != 0)
         {
             Instantiate(gameObject);
             secondsSinceSpawn = 0.0f;
